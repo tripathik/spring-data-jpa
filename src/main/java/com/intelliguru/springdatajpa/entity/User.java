09bf681e0215c3aa82f8dbc409439f2318dadca3
KRISHNA TRIPATHI <bktripathi1977@gmail.com>
2025-01-06 17:53:25 +0530

    Implemented the Swagger UI.

--- src/main/java/com/intelliguru/springdatajpa/entity/User.java
+++ src/main/java/com/intelliguru/springdatajpa/entity/User.java
@@ -1,24 +1,23 @@
-package com.intelliguru.springdatajpa.entity;
-
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
-
-import java.util.List;
-
-@Getter
-@Setter
-@Entity
-@Table(name = "users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NonNull
-    private String userName;
-    private String email;
-    private String password;
-    private List<String> roles;
-}
+//package com.intelliguru.springdatajpa.entity;
+//import lombok.Getter;
+//import lombok.NonNull;
+//import lombok.Setter;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@Getter
+//@Setter
+//@Entity
+//@Table(name = "users")
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    @NonNull
+//    private String userName;
+//    private String email;
+//    private String password;
+//    private List<String> roles;
+//}
--- src/main/java/com/intelliguru/springdatajpa/repository/UserRepository.java
+++ src/main/java/com/intelliguru/springdatajpa/repository/UserRepository.java
@@ -1,8 +1,8 @@
-package com.intelliguru.springdatajpa.repository;
-
-import com.intelliguru.springdatajpa.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserName(String userName);
-}
+//package com.intelliguru.springdatajpa.repository;
+//
+//import com.intelliguru.springdatajpa.entity.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//    User findByUserName(String userName);
+//}
